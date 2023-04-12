d6a64daaaa4f1b37dcffa19f9a9eaf38ec4eccef
khusan9889 <k.xusan_2003@mail.ru>
2023-04-12 18:52:10 +0500

    learning of migrations and models

---
+++ database/migrations/2023_04_12_131857_add_columns_to_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('vendor_id')->nullable()->constrained('vendors')->nullOnDelete();
+            $table->foreignId('role_id')->nullable()->constrained('roles')->nullOnDelete();
+            $table->string('username')->nullable();
+            $table->string('password')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('patronymic')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('vendor_id');
+            $table->dropColumn('role_id');
+            $table->dropColumn('username');
+            $table->dropColumn('password');
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('patronymic');
+            $table->dropColumn('phone_number');
+        });
+    }
+};
